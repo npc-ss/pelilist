50e24c8ec5b138226753d1c5f1d26dcc8c35e1a9
zzair <zairortiz80@gmail.com>
2024-08-23 20:43:44 -0300

    FUNCIONAA FUNCIONAAAA ESTÄ VIVOOO

--- componente/Boton.js
+++ componente/Boton.js
@@ -1,20 +1,43 @@
-import { TouchableOpacity} from "react-native-web";
+import { TouchableOpacity } from "react-native-web";
 import React from "react";
-import { StyleSheet, Text} from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Boton(){
-    return(
+export default function Boton() {
+    return (
         <TouchableOpacity style={styles.container}>
-                <LinearGradient
+            <LinearGradient
                 // Button Linear Gradient
                 colors={['pink', 'white']}
-                start= {{x:0, y:0}}
-                end= {{x:1, y:1}}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
                 style={styles.button}
-                >
+            >
                 <Text style={styles.text}>Ingresar</Text>
-                </LinearGradient>
+            </LinearGradient>
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        width: 200,
+        marginTop: 50,
+    },
+    text:{
+        fonSize: 14,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    button: {
+        width: '60%',
+        height: 40,
+        borderRadius: 20,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+});
