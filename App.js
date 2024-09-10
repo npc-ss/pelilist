97284f076dacddfd035f33b5fba98d4e829f192e
Enzo Coria <coria.enzo.epet20@gmail.com>
2024-09-10 20:40:54 -0300

    mi parte

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Boton from "./componente/Boton";
 import Login from './pantallas/Login';
 import Register from './pantallas/Register';
 import Home from './pantallas/Home';
 import Preferencias from './pantallas/Preferencias';
+import SplashScreen from './pantallas/SplashScreenView';
 import { NavigationContainer } from '@react-navigation/native';
 import  {createStackNavigator} from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
+import { useState } from 'react';
 
 const Stack= createNativeStackNavigator();
 
 
 function App() {
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplash(false);
+    }, 3000);
+  });
   return (
-    <NavigationContainer>
+    <>
+    {isShowSplash ? 
+      (<SplashScreen />
+    ) : (
+      <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/> 
@@ -24,6 +37,8 @@ function App() {
         <Stack.Screen name="Preferencias" component={Preferencias} options={{headerShown: false}}/> 
       </Stack.Navigator>
     </NavigationContainer>
+    )}
+    </>
   );
 }
 export default App;
---
+++ pantallas/SplashScreenView.js
@@ -0,0 +1,55 @@
+import { StyleSheet, View } from "react-native";
+import WubiLogo from '../assets/WubiSplashLogo';
+import React, { useState, useEffect } from 'react';
+import { Animated } from 'react-native';
+import { Easing } from 'react-native';
+
+export default function SplashScreen(){
+  const logoOpacity = new Animated.Value(0.5);
+  const logoScale = new Animated.Value(8);
+
+  useEffect(() => {
+    Animated.timing(logoOpacity, {
+      toValue: 1,
+      duration: 500,
+      easing: Easing.inOut(Easing.quad),
+    }).start();
+
+    Animated.timing(logoScale, {
+      toValue: 1,
+      duration: 500,
+      easing: Easing.inOut(Easing.quad),
+    }).start();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.center}>
+        <View>
+        <Animated.View
+  style={{
+    opacity: logoOpacity,
+    transform: [{ scale: logoScale }],
+  }}
+  useNativeDriver={true}
+>
+  <WubiLogo />
+</Animated.View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#A3966A"
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
